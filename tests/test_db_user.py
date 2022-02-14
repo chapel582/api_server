d6407d677cf24182bd911ba06cc15b2cadaa942c
Trevor Apple <trappleco@gmail.com>
2022-02-13 20:11:19 -0800

    added one more user update db tests

--- tests/test_db_user.py
+++ tests/test_db_user.py
@@ -285,6 +285,8 @@ def test_update_user_phone() -> None:
     user_data: CommonUserData = CommonUserData(
         "test update user function with phone argument"
     )
+    new_phone: str = "0000000"
+    assert new_phone != user_data.phone
 
     try:
         user_created: bool
@@ -297,8 +299,6 @@ def test_update_user_phone() -> None:
         )
         check_created_user(user_created, user_data, created_user_data)
 
-        new_phone: str = "0000000"
-        assert new_phone != user_data.phone
         user_updated: bool
         user_updated = update_user(
             user_data.phone_prefix, user_data.phone, new_phone=new_phone
@@ -315,12 +315,17 @@ def test_update_user_phone() -> None:
         assert get_user_data_check[0] == expected_user_data
     finally:
         delete_user(user_data.phone_prefix, user_data.phone)
+        delete_user(user_data.phone_prefix, new_phone)
 
 
 def test_update_user_prefix_and_phone() -> None:
     user_data: CommonUserData = CommonUserData(
         "test update user function with phone argument"
     )
+    new_phone: str = "0000000"
+    new_prefix: str = "+009"
+    assert new_phone != user_data.phone
+    assert new_prefix != user_data.phone_prefix
 
     try:
         user_created: bool
@@ -333,9 +338,6 @@ def test_update_user_prefix_and_phone() -> None:
         )
         check_created_user(user_created, user_data, created_user_data)
 
-        new_phone: str = "0000000"
-        new_prefix: str = "+009"
-        assert new_phone != user_data.phone
         user_updated: bool
         user_updated = update_user(
             user_data.phone_prefix,
@@ -356,6 +358,68 @@ def test_update_user_prefix_and_phone() -> None:
         assert get_user_data_check[0] == expected_user_data
     finally:
         delete_user(user_data.phone_prefix, user_data.phone)
+        delete_user(new_prefix, new_phone)
+
+
+def test_update_user_all_args() -> None:
+    user_data: CommonUserData = CommonUserData(
+        "test update user function with all arguments"
+    )
+    new_user_name: str = "new user name"
+    new_jwt_token: str = "new fake jwt token"
+    new_pw_hash: str = "new fake pw hash"
+    new_phone: str = "0000000"
+    new_prefix: str = "+009"
+    assert new_user_name != user_data.user_name
+    assert new_phone != user_data.phone
+    assert new_prefix != user_data.phone_prefix
+    assert new_pw_hash != user_data.pw_hash
+
+    org_name: str = "test org"
+    try:
+        org_created: bool
+        org_data: Dict[str, Any]
+        org_created, org_data = create_org(org_name)
+        assert org_created
+
+        user_created: bool
+        created_user_data: Dict[str, Any]
+        user_created, created_user_data = create_user(
+            phone_prefix=user_data.phone_prefix,
+            phone=user_data.phone,
+            user_name=user_data.user_name,
+            pw_hash=user_data.pw_hash,
+        )
+        check_created_user(user_created, user_data, created_user_data)
+
+        user_updated: bool
+        user_updated = update_user(
+            user_data.phone_prefix,
+            user_data.phone,
+            new_phone=new_phone,
+            new_phone_prefix=new_prefix,
+            user_name=new_user_name,
+            pw_hash=new_pw_hash,
+            jwt=new_jwt_token,
+            org_id=org_data["id"],
+        )
+        assert user_updated
+
+        expected_user_data: Dict[str, Any] = created_user_data.copy()
+        expected_user_data["phone"] = new_phone
+        expected_user_data["phone_prefix"] = new_prefix
+        expected_user_data["user_name"] = new_user_name
+        expected_user_data["org_id"] = org_data["id"]
+
+        got_user: bool
+        get_user_data_check: List[Dict[str, Any]]
+        got_user, get_user_data_check = get_user(user_id=created_user_data["id"])
+        assert got_user
+        assert get_user_data_check[0] == expected_user_data
+    finally:
+        delete_user(user_data.phone_prefix, user_data.phone)
+        delete_user(new_prefix, new_phone)
+        delete_org(org_name)
 
 
 def test_delete_user() -> None:
